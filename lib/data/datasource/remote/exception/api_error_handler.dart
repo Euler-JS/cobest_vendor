efefa853f0a9abb188bb209f1eac25c4d58e2ebe
Euler-JS <joaoalbertojose96@gmail.com>
2025-07-31 13:02:21 +0200

    Minimamente. ajustado os pacotes....

--- lib/data/datasource/remote/exception/api_error_handler.dart
+++ lib/data/datasource/remote/exception/api_error_handler.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
-import 'package:sixvalley_vendor_app/data/model/response/base/error_response.dart';
-import 'package:sixvalley_vendor_app/features/auth/controllers/auth_controller.dart';
-import 'package:sixvalley_vendor_app/main.dart';
+import 'package:cobes_marketplace_vendor/data/model/response/base/error_response.dart';
+import 'package:cobes_marketplace_vendor/features/auth/controllers/auth_controller.dart';
+import 'package:cobes_marketplace_vendor/main.dart';
 
 
 class ApiErrorHandler {
